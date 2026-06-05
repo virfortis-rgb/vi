7bf39c406b7f58d72eeae37d1d5efc13c3b3ce8c
Damien <damienusvirfortis@gmail.com>
2026-06-05 15:49:46 +0900

    update hero class

--- lib/hero.rb
+++ lib/hero.rb
@@ -1,12 +1,23 @@
 require 'ruby2d'
 
-class Hero < Sprite
-  def initialize
-    super(
-      x: 20,
-      y: 80,
-      size: 32,
+class Hero
+  def initialize(start_x, start_y, tile_size)
+    @grid_x = start_x
+    @grid_y = start_y
+    @tile_size = tile_size
+    @sprite = Square.new(
+      x: @grid_x * @tile_size,
+      y: @grid_x * @tile_size,
+      size: @tile_size,
       color: 'red'
     )
+  end
+
+  def move_to(target_x, target_y)
+    @grid_x = target_x
+    @grid_y = target_y
+
+    @sprite.x = @grid_x * @tile_size
+    @sprite.y = @grid_y * @tile_size
   end
 end
