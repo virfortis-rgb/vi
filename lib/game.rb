d45851a4af5e17cb72f4bb24b74af7d725747273
Damien <damienusvirfortis@gmail.com>
2026-06-05 15:59:54 +0900

    update game logic

--- lib/game.rb
+++ lib/game.rb
@@ -1,33 +1,28 @@
 class Game
 
-  def initialize(attrs = {})
-    @attrs = attrs
-    reset!
+  def initialize
+    @mundus = Mundus.new
+    @hero = Hero.new(1, 1, @map.tile_size)
+    @mundus.draw
+    setup_inputs
   end
 
-  def reset!
-    @started = false
-    @completed = false
-    @paused = false
+  def setup_inputs
+    on :key_down do |event|
+      next_x = @hero.grid_x
+      next_y = @hero.grid_y
+
+      case event.key
+      when 'up'    then next_y -= 1
+      when 'down'  then next_y += 1
+      when 'left'  then next_x -= 1
+      when 'right' then next_x += 1
+      end
+
+      # Ask the map instance if the destination is safe
+      if @mundus.walkable?(next_x, next_y)
+        @hero.move_to(next_x, next_y)
+      end
+    end
   end
-
-  def start!
-    @start ||= true
-  end
-
-  alias started? started
-
-  def completed
-    @completed = true
-  end
-
-  alias completed? completed
-
-  def paused
-    @paused = !paused?
-  end
-
-  alias paused? paused
-
-# TODO: a method for saving ??
 end
