9b3d769c71af752a7db0e50114cd3ae037a65707
Damien <damienusvirfortis@gmail.com>
2026-06-05 15:21:08 +0900

    update game and hero

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,33 @@
 class Game
-  
+
+  def initialize(attrs = {})
+    @attrs = attrs
+    reset!
+  end
+
+  def reset!
+    @started = false
+    @completed = false
+    @paused = false
+  end
+
+  def start!
+    @start ||= true
+  end
+
+  alias started? started
+
+  def completed
+    @completed = true
+  end
+
+  alias completed? completed
+
+  def paused
+    @paused = !paused?
+  end
+
+  alias paused? paused
+
+# TODO: a method for saving ??
 end
--- lib/hero.rb
+++ lib/hero.rb
@@ -1,5 +1,12 @@
-class Hero
-  def initialize(attrs = {})
-    @attrs = attrs
+require 'ruby2d'
+
+class Hero < Sprite
+  def initialize
+    super(
+      x: 20,
+      y: 80,
+      size: 32,
+      color: 'red'
+    )
   end
 end
