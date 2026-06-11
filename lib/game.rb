21022addb0fff0f8093a5f1ca42970d63ed5020e
Damien <227386375+virfortis-rgb@users.noreply.github.com>
2026-06-11 10:45:12 +0900

    remove some comments

--- lib/game.rb
+++ lib/game.rb
@@ -65,7 +65,6 @@ class Game
   def handle_movement(key)
     next_x = @hero.grid_x
     next_y = @hero.grid_y
-
     case key
     when 'left'
       next_x -= 1
@@ -212,9 +211,7 @@ class Game
   end
 
   def refresh_camera
-    # 1. Tell mundus to shift its tiles around the hero, and get back the map's camera coordinates
     camera_offsets = @camera.update(@hero.grid_x, @hero.grid_y)
-    # 2. Tell the hero to draw himself relative to those camera coordinates
     @hero.update_sprite_viewport(camera_offsets[0], camera_offsets[1])
     @orbes.each { |orb| orb.update_sprite_viewport(camera_offsets[0], camera_offsets[1]) }
     if @libellum
