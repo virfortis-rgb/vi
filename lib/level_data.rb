6c4f37a09deb92a6dfc0e458272b32f4ce19d69d
Damien <227386375+virfortis-rgb@users.noreply.github.com>
2026-06-11 16:00:18 +0900

    add level 3

--- lib/level_data.rb
+++ lib/level_data.rb
@@ -33,6 +33,27 @@ module LevelData
         {
         x: 0, y: 7,
         target_level: 2,
+        spawn_x: 63, spawn_y: 7 
+        }
+      ]
+    },
+        3 => {
+      orbes: [
+        { x: 5, y: 3, verbum: 'cano' },
+        { x: 12, y: 5, verbum: 'verum' },
+        { x: 3, y: 15, verbum: 'et' },
+        { x: 23, y: 21, verbum: 'arma' },
+      ],
+      libellum: {},
+      portals: [
+        {
+        x: 63, y: 7,
+        target_level: 1,
+        spawn_x: 1, spawn_y: 7 
+        },
+        {
+        x: 0, y: 7,
+        target_level: 2,
         spawn_x: 50, spawn_y: 8 
         }
       ]
